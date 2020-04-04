423a2408e631e9517d712d696d19c2ce7d65e70b
Akron254 <aaronrono42@gmail.com>
2020-04-04 19:35:15 +0300

    Created data class model

--- app/src/main/java/com/kenova/clipboard/MainActivity.kt
+++ app/src/main/java/com/kenova/clipboard/MainActivity.kt
@@ -2,11 +2,14 @@ package com.kenova.clipboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
---
+++ app/src/main/java/com/kenova/clipboard/models/Model.kt
@@ -0,0 +1,7 @@
+package com.kenova.clipboard.models
+
+data class Clip(var title: String, var content: String)
+
+object Supplier {
+    val clips = listOf<Clip>()
+}
