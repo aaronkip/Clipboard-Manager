0a771fd3b5718b80407d9eb1de49304d6435d821
Akron254 <aaronrono42@gmail.com>
2020-04-04 20:51:11 +0300

    Added Clipboard service

--- app/src/main/java/com/kenova/clipboard/MainActivity.kt
+++ app/src/main/java/com/kenova/clipboard/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.kenova.clipboard
 
+import android.content.ClipboardManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.getSystemService
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.kenova.clipboard.adapters.ClipAdapter
+import com.kenova.clipboard.models.Clip
 import com.kenova.clipboard.models.Supplier
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -20,6 +24,12 @@ class MainActivity : AppCompatActivity() {
 
         val adapter = ClipAdapter(this, Supplier.clips)
         recyclerView.adapter = adapter
+
+        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        clipboard.addPrimaryClipChangedListener {
+            val textToPaste: String = clipboard.primaryClip?.getItemAt(0)?.text.toString().trim()
+            Supplier.clips.toMutableList().add(Clip(textToPaste,""))
+        }
 
     }
 }
--- app/src/main/java/com/kenova/clipboard/models/Model.kt
+++ app/src/main/java/com/kenova/clipboard/models/Model.kt
@@ -1,6 +1,8 @@
 package com.kenova.clipboard.models
 
-data class Clip(var title: String, var content: String)
+data class Clip(var title: String, var content: String) {
+
+}
 
 object Supplier {
     val clips = listOf<Clip>()
