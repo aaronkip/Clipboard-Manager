64a376d64503e5e9c9e8f733b74f986745f58017
Akron254 <aaronrono42@gmail.com>
2020-04-12 06:58:17 +0300

    Dummy clip content

--- app/src/main/java/com/kenova/clipboard/models/Model.kt
+++ app/src/main/java/com/kenova/clipboard/models/Model.kt
@@ -6,8 +6,8 @@ data class Clip(var title: String, var content: String) {
 
 public object Supplier {
     val clips = listOf<Clip>(
-        Clip("ClipData1",""),
-        Clip("ClipData2",""),
-        Clip("ClipData3","")
+        Clip("ClipData1","This is the content of the first index clipboard"),
+        Clip("ClipData2","Dummy content of the second index clipboard"),
+        Clip("ClipData3","Example data for the third index clipboard")
     )
 }
