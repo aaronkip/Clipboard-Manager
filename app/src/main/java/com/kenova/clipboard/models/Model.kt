226981ac9c66c529b0d746ce436f15ee67f1ca6e
Akron254 <aaronrono42@gmail.com>
2020-04-15 21:53:47 +0300

    Demo text lengthen

--- app/src/main/java/com/kenova/clipboard/models/Model.kt
+++ app/src/main/java/com/kenova/clipboard/models/Model.kt
@@ -11,6 +11,6 @@ public object Supplier {
 
         Clip("ClipData1","This is the content of the first index clipboard"),
         Clip("ClipData2","Dummy content of the second index clipboard"),
-        Clip("ClipData3","Example data for the third index clipboard")
+        Clip("ClipData3","Example data for the third index clipboard, extending this to a more longer text will be worthwhile in testing the text support height of the recycler view item. Hope this is long enough")
     )
 }
